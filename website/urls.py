309ec63b0d348a525aebeda5b38a78e238f764a6
sanch27 <sanchaliannavarapu@gmail.com>
2024-03-02 02:39:43 +0530

    inital commit

---
+++ website/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    #path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout'),
+    path('register/', views.register_user, name='register'),
+    path('record/<int:pk>', views.customer_record, name='record'),
+    path('delete_record/<int:pk>', views.delete_record, name='delete_record'),
+    path('add_record/', views.add_record, name='add_record'),
+    path('update_record/<int:pk>', views.update_record, name='update_record'),
+    path('course/<int:pk>', views.customer_course, name='course'),
+    path('delete_course/<int:pk>', views.delete_course, name='delete_course'),
+    path('add_course/', views.add_course, name='add_course'),
+    path('update_course/<int:pk>', views.update_course, name='update_course'),
+]
